3bea6f4326de0abe18838ec401fd96bf581d0ae0
Always Hungry <suico.dave@gmail.com>
2018-02-27 23:07:49 +0800

    staging api

--- src/app/interfaces/global.ts
+++ src/app/interfaces/global.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 
 
-export const apiUrl = 'http://localhost:8000/api/';
+export const apiUrl = 'https://sces-api.herokuapp.com/api/';
 
 export const apiHeaders = new HttpHeaders({
     'X-Requested-With': 'XMLHttpRequest',
