4e86de61987b56ee3fa7132bd1acb5231bcdb736
Always Hungry <suico.dave@gmail.com>
2018-02-28 20:02:47 +0800

    api link

--- src/app/interfaces/global.ts
+++ src/app/interfaces/global.ts
@@ -1,11 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 
 
-<<<<<<< HEAD
 export const apiUrl = 'https://sces-api.herokuapp.com/api/';
-=======
-export const apiUrl = 'http://127.0.0.1:8000/api/';
->>>>>>> master
 
 export const apiHeaders = new HttpHeaders({
     'X-Requested-With': 'XMLHttpRequest',
